8093479ee2655a10672d4f7b7ba31d90d6d20c1b
Andrew-Thompson17 <at781419@ohio.edu>
2020-09-03 16:17:18 -0400

    Adding the program

---
+++ git-lab-2-program.cpp
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-2-program.cpp
+ *      Author: Andrew Thompson
+ *        Date: 9/3/20
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "hello git!" << endl;
+
+    return 0;
+}// main
